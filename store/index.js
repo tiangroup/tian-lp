a6afc3bdbbf589a7ae55392051ed607852857605
pavlenkoia <pavlenkoia@gmail.com>
2020-07-31 17:59:30 +0500

    work

--- store/index.js
+++ store/index.js
@@ -1,7 +1,15 @@
 export const actions = {
   async nuxtServerInit({ commit }, { req }) {
     try {
-      const hostname = process.env.SITE_NAME || req.headers.host.split(":")[0];
+      let hostname = process.env.SITE_NAME;
+      // todo: поставить проверку сервера req
+      if (!hostname) {
+        const reg = "(.*).app.tian-lp.ru";
+        const regexp = new RegExp(reg, "ig");
+        hostname = req.headers.host.split(":")[0];
+        const result = regexp.exec(hostname);
+        hostname = result ? result[1] : null;
+      }
       const sites = await this.$axios.$get(`${this.$url_api}/sites`, {
         params: { name: hostname }
       });
