7564239894fbbdf312f375ded4a0345a3199c0d8
lollikay <lottakayumova@gmail.com>
2020-08-07 17:20:22 +0500

    updates

--- components/sections/partners/partners.vue
+++ components/sections/partners/partners.vue
@@ -1,78 +1,162 @@
 <template>
-<div :style="styleDiv" :id="section.id">
+  <div :style="styleDiv" :id="section.id">
     <buttons-section v-if="isEdit" :section="section" />
-    <div class="partners custom-v-spacing bg-primary">
-        <div class="landing__container">
-          <h2>Партнеры</h2>
-          <div class="partners__list cells justify-content-center">
-            <div class="partners__item-wrap cell cell-6 cell-sm-4 cell-md-3 cell-xl-2">
-              <div class="partners__item">
-                <div class="partners__image"><img src="/assets/content-images/partners/p-1.png"></div>
-                <div class="partners__text">Acer</div>
+    <div
+      class="partners custom-v-spacing bg-primary"
+      :class="{ mDark: section.settings.background === 'dark' }"
+    >
+      <div class="landing__container">
+        <h2 v-if="isEdit">
+          <editor :text="section.title || ''" :sectionId="section.id" field="title" />
+        </h2>
+        <h2 v-else>{{ section.title }}</h2>
+        <div
+          class="partners__list cells justify-content-center"
+          v-if="section.items && (isEdit || section.settings.view === 'list')"
+        >
+          <div
+            class="partners__item-wrap cell cell-6 cell-sm-4 cell-md-3 cell-xl-2"
+            v-for="item in section.items.filter(i => i.id)"
+            :key="item.id"
+            :style="styleDiv"
+          >
+            <buttons-item v-if="isEdit" :itemId="item.id" :sectionId="section.id" />
+            <div class="partners__item">
+              <a v-if="item.link && !isEdit" :href="item.link" class="partners__link"></a>
+              <div
+                class="partners__image"
+                :class="{ 'no-image': !item.img }"
+                :title="isEdit ? 'Двойной клик - изменить картинку' : ''"
+                @dblclick="
+                  itemImageSelect({
+                    itemId: item.id,
+                    field: 'img',
+                    value: item.img
+                  })
+                "
+              >
+                <img v-if="item.img" :src="$site_img(item.img)" />
               </div>
-            </div>
-            <div class="partners__item-wrap cell cell-6 cell-sm-4 cell-md-3 cell-xl-2">
-              <div class="partners__item">
-                <div class="partners__image">
-                  <a href="" target="_blank"><img src="/assets/content-images/partners/p-2.png"></a>
-                </div>
-                <div class="partners__text">Локомотив-Кубань</div>
+              <div class="partners__text" v-if="isEdit">
+                <editor
+                  :text="item.title || ''"
+                  :sectionId="section.id"
+                  field="title"
+                  :itemId="item.id"
+                />
               </div>
-            </div>
-            <div class="partners__item-wrap cell cell-6 cell-sm-4 cell-md-3 cell-xl-2">
-              <div class="partners__item">
-                <div class="partners__image"><img src="/assets/content-images/partners/p-3.png"></div>
-                <div class="partners__text">
-                  <a href="" target="_blank">Starbucks</a>
-                </div>
-              </div>
-            </div>
-            <div class="partners__item-wrap cell cell-6 cell-sm-4 cell-md-3 cell-xl-2">
-              <div class="partners__item">
-                <a href="" target="_blank">
-                  <div class="partners__image"><img src="/assets/content-images/partners/p-4.png"></div>
-                  <div class="partners__text">Газпром</div>
-                </a>
-              </div>
-            </div>
-            <div class="partners__item-wrap cell cell-6 cell-sm-4 cell-md-3 cell-xl-2">
-              <div class="partners__item">
-                <div class="partners__image"><img src="/assets/content-images/partners/p-5.png"></div>
-                <div class="partners__text">DPD</div>
-              </div>
-            </div>
-            <div class="partners__item-wrap cell cell-6 cell-sm-4 cell-md-3 cell-xl-2">
-              <div class="partners__item">
-                <div class="partners__image"><img src="/assets/content-images/partners/p-6.png"></div>
-                <div class="partners__text">Supreme</div>
-              </div>
-            </div>
-            <div class="partners__item-wrap cell cell-6 cell-sm-4 cell-md-3 cell-xl-2">
-              <div class="partners__item">
-                <div class="partners__image"><img src="/assets/content-images/partners/p-7.png"></div>
-                <div class="partners__text">Dolce & Gabbana</div>
-              </div>
+              <div v-else class="partners__text">{{ item.title }}</div>
             </div>
           </div>
+          <div
+            class="partners__item-wrap cell cell-6 cell-sm-4 cell-md-3 cell-xl-2"
+            v-if="isEdit && (!section.items || !section.items.length)"
+          >
+            <buttons-item-add :sectionId="section.id" />
+          </div>
+        </div>
+        <div v-else>
+          <slick ref="slick" :options="slickOptions" class="partners__list">
+            <div
+              class="partners__item-wrap"
+              v-for="item in section.items.filter(i => i.id)"
+              :key="item.id"
+            >
+              <div class="partners__item">
+                <a v-if="item.link" :href="item.link" class="partners__link"></a>
+                <div
+                  class="partners__image"
+                  :class="{ 'no-image': !item.img }"
+                >
+                  <img v-if="item.img" :src="$site_img(item.img)" />
+                </div>
+                <div class="partners__text">{{ item.title }}</div>
+              </div>
+            </div>
+          </slick>
         </div>
       </div>
-</div>
+      <image-upload
+        v-if="isEdit"
+        :dialog="dialogImageUpload"
+        :itemImageEdit="itemImageEdit"
+        @close="dialogImageUpload = false"
+        @onUpload="onUploadImage"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
 import { mapMutations, mapGetters } from "vuex";
 export default {
-props: {
-    section: Object
+  props: {
+    section: Object,
   },
-computed: {
+  data: () => ({
+    dialogImageUpload: false,
+    itemImageEdit: {},
+    slickOptions: {
+      arrows: true,
+      dots: true,
+      infinite: true,
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      prevArrow: '<button type="button" class="slick-arrow slick-prev"><svg width="17" height="28" viewBox="0 0 17 28" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16 1L2 13.9706L15.966 27" stroke="currentColor" stroke-width="2" stroke-linecap="round"></path></svg></button>',
+      nextArrow: '<button type="button" class="slick-arrow slick-next"><svg width="17" height="28" viewBox="0 0 17 28" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1L15 13.9706L1.03398 27" stroke="currentColor" stroke-width="2" stroke-linecap="round"></path></svg></button>',
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            arrows: false
+          }
+        },
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            arrows: false
+          }
+        }
+      ]
+    },
+  }),
+  computed: {
     ...mapGetters({
-      isEdit: "isEdit"
+      isEdit: "isEdit",
     }),
     styleDiv() {
       return this.isEdit ? { position: "relative" } : null;
-    }
-  }
-}
-  
+    },
+  },
+  methods: {
+    ...mapMutations({
+      setItemField: "pages/SET_ITEM_FIELD",
+    }),
+    itemImageSelect(item) {
+      this.itemImageEdit = item;
+      this.dialogImageUpload = true;
+    },
+    onUploadImage(payload) {
+      this.dialogImageUpload = false;
+      this.setItemField({
+        sectionId: this.section.id,
+        itemId: payload.itemId,
+        items: "items",
+        field: payload.field,
+        value: payload.value,
+      });
+      this.$store.dispatch("pages/savePage");
+    },
+    onDeleteItem(payload) {
+      this.$emit("onItemDelete", payload);
+    },
+  },
+};
 </script>
+<style>
+  @import 'node_modules/slick-carousel/slick/slick.css'
+</style>
