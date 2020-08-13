052590be2a91eb02eb8d9e1c78defdb90cc916f6
lollikay <lottakayumova@gmail.com>
2020-08-13 18:02:39 +0500

    updates. video needs work

--- components/sections/staff/staff.vue
+++ components/sections/staff/staff.vue
@@ -10,91 +10,88 @@
           <editor :text="section.title || ''" :sectionId="section.id" field="title" />
         </h2>
         <h2 v-else>{{ section.title }}</h2>
-        <slick
-          ref="slick"
-          :options="updatedSlickOptions"
-          class="staff__list"
-          v-if="section.items && isSlick"
-        >
-          <div
-            class="staff__item-wrap cell"
-            :class="{'position-relative': isEdit}"
-            v-for="item in section.items.filter(i => i.id)"
-            :key="item.id"
-          >
-            <buttons-item
-              v-if="isEdit"
-              :itemId="item.id"
-              :sectionId="section.id"
-              @onAction="onItemsChange"
-              @onItemDelete="onItemDelete"
-            />
-            <div class="staff__item">
-              <image-item
-                divClass="staff__image"
-                :img="item.img"
+        <div class="staff__list mx-n15 mx-md-n1rem" v-if="section.items && isSlick">
+          <slick ref="slick" :options="updatedSlickOptions">
+            <div
+              class="staff__item-wrap cell"
+              :class="{'position-relative': isEdit}"
+              v-for="item in section.items.filter(i => i.id)"
+              :key="item.id"
+            >
+              <buttons-item
+                v-if="isEdit"
                 :itemId="item.id"
                 :sectionId="section.id"
+                @onAction="onItemsChange"
+                @onItemDelete="onItemDelete"
               />
-              <div class="staff__info">
-                <div class="staff__name" v-if="isEdit">
-                  <editor
-                    data-placeholder="Имя Фамилия"
-                    :text="item.name || ''"
-                    :sectionId="section.id"
-                    field="name"
-                    :itemId="item.id"
-                  />
+              <div class="staff__item">
+                <image-item
+                  divClass="staff__image"
+                  :img="item.img"
+                  :itemId="item.id"
+                  :sectionId="section.id"
+                />
+                <div class="staff__info">
+                  <div class="staff__name" v-if="isEdit">
+                    <editor
+                      data-placeholder="Имя Фамилия"
+                      :text="item.name || ''"
+                      :sectionId="section.id"
+                      field="name"
+                      :itemId="item.id"
+                    />
+                  </div>
+                  <div v-else class="staff__name">{{ item.name }}</div>
+                  <div class="staff__position" v-if="isEdit">
+                    <editor
+                      data-placeholder="должность"
+                      :text="item.position || ''"
+                      :sectionId="section.id"
+                      field="position"
+                      :itemId="item.id"
+                    />
+                  </div>
+                  <div v-else class="staff__position">{{ item.position }}</div>
                 </div>
-                <div v-else class="staff__name">{{ item.name }}</div>
-                <div class="staff__position" v-if="isEdit">
-                  <editor
-                    data-placeholder="должность"
-                    :text="item.position || ''"
-                    :sectionId="section.id"
-                    field="position"
-                    :itemId="item.id"
-                  />
-                </div>
-                <div v-else class="staff__position">{{ item.position }}</div>
-              </div>
-              <div class="staff__contacts">
-                <div class="staff__phone" v-if="isEdit">
-                  <editor
-                    data-placeholder="+7 351 111-22-33"
-                    :text="item.phone || ''"
-                    editContent="html"
-                    :sectionId="section.id"
-                    field="phone"
-                    :itemId="item.id"
-                  />
-                </div>
-                <div v-else class="staff__phone" v-html="item.phone"></div>
-                <div class="staff__email">
-                  <editor
-                    data-placeholder="mail@mail.ru"
-                    :text="item.email || ''"
-                    :sectionId="section.id"
-                    field="email"
-                    :itemId="item.id"
-                    v-if="isEdit"
-                  />
-                  <a
-                    v-else-if="isValidEmail(item.email)"
-                    :href="`mailto:${item.email}`"
-                  >{{ item.email }}</a>
-                  <span v-else>{{ item.email }}</span>
+                <div class="staff__contacts">
+                  <div class="staff__phone" v-if="isEdit">
+                    <editor
+                      data-placeholder="+7 351 111-22-33"
+                      :text="item.phone || ''"
+                      editContent="html"
+                      :sectionId="section.id"
+                      field="phone"
+                      :itemId="item.id"
+                    />
+                  </div>
+                  <div v-else class="staff__phone" v-html="item.phone"></div>
+                  <div class="staff__email">
+                    <editor
+                      data-placeholder="mail@mail.ru"
+                      :text="item.email || ''"
+                      :sectionId="section.id"
+                      field="email"
+                      :itemId="item.id"
+                      v-if="isEdit"
+                    />
+                    <a
+                      v-else-if="isValidEmail(item.email)"
+                      :href="`mailto:${item.email}`"
+                    >{{ item.email }}</a>
+                    <span v-else>{{ item.email }}</span>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div
-            class="staff__item-wrap cell"
-            v-if="isEdit && (!section.items || !section.items.length)"
-          >
-            <buttons-item-add :sectionId="section.id" />
-          </div>
-        </slick>
+            <div
+              class="staff__item-wrap cell"
+              v-if="isEdit && (!section.items || !section.items.length)"
+            >
+              <buttons-item-add :sectionId="section.id" />
+            </div>
+          </slick>
+        </div>
       </div>
     </div>
   </div>
--- components/sections/video/video.vue
+++ components/sections/video/video.vue
@@ -20,89 +20,82 @@
           />
           <div v-else v-html="section.description"></div>
         </div>
-        <v-gallery
-          :images="images"
-          :index="index"
-          @close="index = null"
-          v-if="!isEdit"
-          :options="{youTubeVideoIdProperty: 'youtube', youTubePlayerVars: undefined, youTubeClickToPlay: true}"
-          :id="'video-gallery-' + section.id"
-        ></v-gallery>
-        <slick
-          ref="slick"
-          :options="updatedSlickOptions"
-          class="video__list"
-          v-if="section.items && isSlick"
-        >
-          <div
-            class="video__item-wrap cell"
-            :class="{'position-relative': isEdit}"
-            v-for="(item, itemIndex) in section.items.filter(i => i.id)"
-            :key="item.id"
-          >
-            <buttons-item
-              v-if="isEdit"
-              :itemId="item.id"
-              :sectionId="section.id"
-              @onAction="onItemsChange"
-            />
-            <component
-              class="video__item"
-              :is="elem"
-              :href="item.link"
-              @click="callGallery(itemIndex)"
+        <div class="mx-ncell" v-if="section.items && isEdit">
+          <slick ref="slick" :options="updatedSlickOptions" class="video__list" v-if="isSlick">
+            <div
+              class="video__item-wrap cell position-relative"
+              v-for="item in section.items.filter(i => i.id)"
+              :key="item.id"
             >
-              <div
-                class="video__cover"
-                :class="{ 'video__cover--editable': isEdit }"
-                :title="isEdit ? 'Клик - изменить ссылку на видео' : ''"
-                @click.stop="
+              <buttons-item :itemId="item.id" :sectionId="section.id" @onAction="onItemsChange" />
+              <div class="video__item">
+                <div
+                  class="video__cover video__cover--editable"
+                  title="Клик - изменить ссылку на видео"
+                  @click.stop="
                   itemVideoInput({
                     itemId: item.id,
                     field: 'link',
                     value: item.link
                   })
                 "
-              >
-                <img v-if="item.link" :src="videoCover(item.link)" />
+                >
+                  <img v-if="item.link" :src="videoCover(getVideoId(item.link))" />
+                </div>
+                <div class="video__title">
+                  <editor
+                    data-placeholder="Название видео"
+                    :text="item.title || ''"
+                    :sectionId="section.id"
+                    field="title"
+                    :itemId="item.id"
+                  />
+                </div>
               </div>
-              <div class="video__title">
-                <editor
-                  data-placeholder="Название видео"
-                  :text="item.title || ''"
-                  :sectionId="section.id"
-                  field="title"
-                  :itemId="item.id"
-                  v-if="isEdit"
-                />
-                <span v-else>{{ item.title }}</span>
+            </div>
+            <div class="video__item-wrap cell" v-if="!section.items || !section.items.length">
+              <buttons-item-add :sectionId="section.id" />
+            </div>
+          </slick>
+
+          <v-dialog v-model="videoUrlDialog" max-width="33rem">
+            <v-card>
+              <v-card-title class="mb-10">
+                Ссылка на Youtube-видео
+                <v-spacer></v-spacer>
+                <v-btn icon @click="videoUrlDialog = false">
+                  <v-icon>mdi-close</v-icon>
+                </v-btn>
+              </v-card-title>
+              <v-card-text>
+                <v-text-field label="Ссылка" outlined v-model="userUrl"></v-text-field>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn depressed color="gray" @click="videoUrlDialog = false">Отменить</v-btn>
+                <v-btn depressed color="green" @click="setVideoUrl(userUrl)">Сохранить</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </div>
+        <div class="mx-ncell" v-else-if="section.items && !isEdit">
+          <slick ref="slick" :options="updatedSlickOptions" class="video__list" v-if="isSlick">
+            <div
+              class="video__item-wrap cell"
+              v-for="(item, itemIndex) in section.items.filter(i => i.id)"
+              :key="item.id"
+            >
+              <div class="video__item">
+                <div class="video__cover">
+                  <img v-if="item.link" :src="videoCover(getVideoId(item.link))" />
+                </div>
+                <div class="video__title">{{ item.title }}</div>
+                <div class="video__id display-none">{{ getVideoId(item.link) }}</div>
+                <div :id="'vid' + itemIndex"></div>
               </div>
-            </component>
-          </div>
-          <div
-            class="video__item-wrap cell"
-            v-if="isEdit && (!section.items || !section.items.length)"
-          >
-            <buttons-item-add :sectionId="section.id" />
-          </div>
-        </slick>
-        <v-dialog v-model="videoUrlDialog" max-width="33rem">
-          <v-card>
-            <v-card-title>Ссылка на Youtube-видео</v-card-title>
-            <v-spacer></v-spacer>
-            <v-btn icon @click="videoUrlDialog = false">
-              <v-icon>mdi-close</v-icon>
-            </v-btn>
-            <v-card-text>
-              <v-text-field label="Ссылка" outlined v-model="userUrl"></v-text-field>
-            </v-card-text>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn depressed color="gray" @click="videoUrlDialog = false">Отменить</v-btn>
-              <v-btn depressed color="green" @click="setVideoUrl(userUrl)">Сохранить</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
+            </div>
+          </slick>
+        </div>
       </div>
     </div>
   </div>
@@ -115,7 +108,6 @@ export default {
     section: Object,
   },
   data: () => ({
-    index: null,
     isSlick: true,
     slickOptions: {
       arrows: true,
@@ -167,20 +159,6 @@ export default {
     elem() {
       return this.isEdit ? "div" : "a";
     },
-    images() {
-      let itemImages = [];
-      for (var i = 0; i < this.section.items.length; i++) {
-        let item = this.section.items[i];
-        let itemImage = {
-          title: item.title,
-          href: item.link,
-          type: "text/html",
-          poster: "https://img.youtube.com/vi/PiDB4L-m6FQ/maxresdefault.jpg",
-        };
-        itemImages.push(itemImage);
-      }
-      return itemImages;
-    },
   },
   methods: {
     ...mapMutations({
@@ -201,11 +179,14 @@ export default {
         _this.isSlick = true;
       }, 100);
     },
-    videoCover(url) {
+    getVideoId(url) {
       const youtubeRegex = /^.*(youtu\.be\/|vi?\/|u\/\w\/|embed\/|\?vi?=|\&vi?=)([^#\&\?]*).*/;
       const youtubeId = url.match(youtubeRegex);
+      return youtubeId[2];
+    },
+    videoCover(videoId) {
       const coverUrl =
-        "https://img.youtube.com/vi/" + youtubeId[2] + "/maxresdefault.jpg";
+        "https://img.youtube.com/vi/" + videoId + "/maxresdefault.jpg";
       return coverUrl;
     },
     setVideoUrl(userUrl) {
@@ -218,12 +199,7 @@ export default {
       });
       this.$store.dispatch("pages/savePage");
     },
-    callGallery(itemIndex) {
-      if (this.isEdit) {
-        return;
-      }
-      index = itemIndex;
-    },
+    playVideo(itemIndex) {},
   },
   watch: {
     isEdit: function () {
@@ -231,6 +207,44 @@ export default {
       this.updatedSlickOptions.draggable = !this.isEdit;
       this.restartSlick();
     },
+  },
+  mounted() {
+    if (!this.isEdit) {
+      var tag = document.createElement("script");
+      tag.src = "https://www.youtube.com/iframe_api";
+      document.querySelector(".video").insertAdjacentElement("afterbegin", tag);
+
+      var videoItems = document.querySelectorAll(".video__item");
+      console.log(videoItems);
+
+      for (var i = 0; i < videoItems.length; i++) {
+        var videoItem = videoItems[i];
+        videoItem.addEventListener("click", function handler() {
+          this.classList.add("video__item--active");
+          this.querySelector(".video__cover").classList.add("display-none");
+          this.querySelector(".video__title").classList.add("display-none");
+          var videoId = this.querySelector(".video__id").innerHTML;
+          var videoUrl = "https://www.youtube.com/embed/" + videoId;
+
+          var player;
+          function onYouTubeIframeAPIReady() {
+            player = new YT.Player("vid" + i, {
+              height: "360",
+              width: "640",
+              videoId: videoId,
+              events: {
+                onReady: onPlayerReady,
+              },
+            });
+          }
+
+          function onPlayerReady(event) {
+            event.target.playVideo();
+          }
+          this.removeEventListener("click", handler);
+        });
+      }
+    }
   },
 };
 </script>
