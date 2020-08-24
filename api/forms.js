1a809737fac3f15382086e318bb1d665881c2748
pavlenkoia <pavlenkoia@gmail.com>
2020-08-24 12:07:55 +0500

    work

--- api/forms.js
+++ api/forms.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const axios = require("axios");
 const fileUpload = require("express-fileupload");
+const nodemailer = require("nodemailer");
 
 const api_backend = process.env.API_BACKEND;
 const admin_token = process.env.ADMIN_TOKEN;
@@ -16,14 +17,53 @@ app.use(
 );
 
 app.post("/", async (req, res) => {
-  const id = req.body.id;
-  console.log(req.body);
-  const { data: form } = await axios.get(`${api_backend}/forms/${id}`, {
-    params: {
-      token: admin_token
+  try {
+    // получение формы
+    const id = req.body.id;
+    const { data: form } = await axios.get(`${api_backend}/forms/${id}`, {
+      params: {
+        token: admin_token
+      }
+    });
+    if (form.mail && form.mail.to) {
+      // разбор полей
+      const fields = Array();
+      for (let field of form.fields) {
+        console.log(field.label + ": " + req.body[field.id]);
+        fields.push({
+          label: field.label,
+          value: req.body[field.id],
+          type: field.type
+        });
+      }
+
+      // отправка сообщения
+      let testAccount = await nodemailer.createTestAccount();
+      let transporter = nodemailer.createTransport({
+        host: "smtp.ethereal.email",
+        port: 587,
+        secure: false,
+        auth: {
+          user: testAccount.user,
+          pass: testAccount.pass
+        }
+      });
+      let info = await transporter.sendMail({
+        from: '"Лендинг" <noreply@tian-lp.ru>',
+        to: form.mail.to,
+        subject: form.mail.subject,
+        text: "Hello world?"
+        //html: "<b>Hello world?</b>"
+      });
+      console.log("Message sent: %s", info.messageId);
+      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
     }
-  });
-  res.status(200).send(form);
+
+    res.status(200).send("OK");
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
 });
 
 module.exports = {
