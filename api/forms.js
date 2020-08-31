22217da30c69ecf1b9d56ef89266de6f21445b0f
pavlenkoia <pavlenkoia@gmail.com>
2020-08-31 15:51:35 +0500

    work

--- api/forms.js
+++ api/forms.js
@@ -38,6 +38,7 @@ app.post("/", async (req, res) => {
           type: field.type
         });
       }
+
       const mailTextTpl = "{{#fields}}{{../field}}{{/fields}}";
       const mailText = templayed(mailTextTpl)({
         fields,
@@ -49,6 +50,7 @@ app.post("/", async (req, res) => {
           return value ? `${this.label}: ${value}\n` : null;
         }
       });
+
       // отправка сообщения
       let testAccount = await nodemailer.createTestAccount();
       let transporter = nodemailer.createTransport({
