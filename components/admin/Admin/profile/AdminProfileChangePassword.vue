266f6b72d31cb1a42005e84e69bab70e36c0395f
Igor A. Pavlenko <pavlenkoia@gmail.com>
2020-11-02 18:29:47 +0500

    profile add

--- components/admin/Admin/profile/AdminProfileChangePassword.vue
+++ components/admin/Admin/profile/AdminProfileChangePassword.vue
@@ -2,14 +2,44 @@
   <v-card>
     <v-card-title>Изменить пароль</v-card-title>
     <v-card-text>
-      <v-text-field label="Старый пароль" />
-      <v-text-field label="Новый пароль" />
+      <v-text-field
+        label="Старый пароль"
+        type="password"
+        v-model.trim="oldPassword"
+      />
+      <v-text-field
+        label="Новый пароль"
+        v-model.trim="newPassword"
+        :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+        @click:append="showPassword = !showPassword"
+        :type="showPassword ? 'text' : 'password'"
+      />
     </v-card-text>
     <v-card-actions>
       <v-spacer></v-spacer>
-      <v-btn color="blue darken-1" text @click="save">
+      <v-btn color="blue darken-1" text :disabled="disabled" :loading="process">
         Изменить
       </v-btn>
     </v-card-actions>
   </v-card>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    process: false,
+    oldPassword: null,
+    newPassword: null,
+    showPassword: false
+  }),
+  computed: {
+    disabled() {
+      return (
+        !this.oldPassword ||
+        !this.newPassword ||
+        this.oldPassword === this.newPassword
+      );
+    }
+  }
+};
+</script>
