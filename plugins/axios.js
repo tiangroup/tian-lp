740bb4d75e12292800da119fb8aef29d34a2b990
Igor A. Pavlenko <pavlenkoia@gmail.com>
2020-11-09 17:32:27 +0500

    axios fix

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,7 +1,7 @@
 export default function({ $axios, store, app }) {
   $axios.onError(error => {
     const code = parseInt(error.response && error.response.status);
-    if (code === 401) {
+    if (code === 401 || code === 403) {
       app.$auth.logout();
       //window.location.reload();
       //store.commit("auth/SET", { key: "loggedIn", value: false });
